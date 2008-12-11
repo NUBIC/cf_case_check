10ae42d47d3ceb58a148f2710dc227660d12a00d
Rhett Sutphin <rhett@detailedbalance.net>
2008-12-10 18:09:18 -0600

    Add <cfmodule template='...' support

--- lib/case_check/references/cfmodule.rb
+++ lib/case_check/references/cfmodule.rb
@@ -2,13 +2,15 @@ module CaseCheck
   
   # Reference as <cfmodule name= or <cfmodule template=
   class Cfmodule < Reference
+    attr_reader :expected_path, :resolved_to
+
     class << self
       def search(source)
         source.scan_for_tag('cfmodule') do |text, attributes, line_number|
           if attributes.keys.include?(:name)
             Name.new(source, attributes[:name], line_number)
-          # elsif attributes.keys.include?(:template)
-          #   Template.new(source, attributes[:template], line_number)
+          elsif attributes.keys.include?(:template)
+            Template.new(source, attributes[:template], line_number)
           else
             $stderr.puts "Neither name nor template for cfmodule on line #{line_number} of #{source.filename}"
           end
@@ -17,8 +19,6 @@ module CaseCheck
     end
     
     class Name < Cfmodule
-      attr_reader :expected_path, :resolved_to
-      
       def initialize(source, text, line)
         super
         @expected_path = text.gsub('.', '/') + ".cfm"
@@ -33,6 +33,15 @@ module CaseCheck
     end
     
     class Template < Cfmodule
+      def initialize(source, text, line)
+        super
+        @expected_path = text
+        @resolved_to = resolve_in(File.dirname(source.filename))
+      end
+      
+      def type_name
+        "cfmodule with template"
+      end
     end
   end
   
--- spec/references/cfmodule_spec.rb
+++ spec/references/cfmodule_spec.rb
@@ -78,3 +78,67 @@ describe CaseCheck::Cfmodule::Name do
     actual_search.should have(1).reference
   end
 end
+
+describe CaseCheck::Cfmodule::Template do
+  before(:each) do
+    # cfmodule with the name attribute searches in the custom tag directories
+    CaseCheck::CustomTag.directories = %w(/tmp/cfmod_specs/customtags)
+    CaseCheck::CustomTag.directories.each { |d| FileUtils.mkdir_p d }
+    
+    @source = create_test_source("/tmp/cfmod_specs/theapp/quux.cfm", <<-CFM)
+      <CFOUTPUT>
+              <CFIF StoredQueryID GT 0>
+              storedqueryid : #StoredQueryID#
+                      <CFModule Template="GetTheStoredStatement.cfm" datasource="#tablespace#" QueryName="#StoredQueryName#" 
+                              category="#querycategory#"  sq_id=#StoredQueryID#>
+                      <CFSET sqlstatement=TheStoredStatement>
+              </CFIF>
+      </CFOUTPUT>
+    CFM
+  end
+  
+  after(:each) do
+    FileUtils.rm_r '/tmp/cfmod_specs'
+  end
+  
+  def actual_search
+    CaseCheck::Cfmodule.search(@source)
+  end
+  
+  it "has a human-readable name" do
+    actual_search.first.type_name.should == 'cfmodule with template'
+  end
+  
+  it "uses the template path as the path" do
+    actual_search.first.expected_path.should == "GetTheStoredStatement.cfm"
+  end
+  
+  it "resolves against file directory" do
+    expected_file = "/tmp/cfmod_specs/theapp/GetTheStoredStatement.cfm"
+    touch expected_file
+    actual_search.first.resolved_to.should == expected_file
+    actual_search.first.resolution.should == :exact
+  end
+  
+  it "resolves against file directory case-insensitively" do
+    expected_file = "/tmp/cfmod_specs/theapp/GetTheSTOREDStatement.cfm"
+    touch expected_file
+    actual_search.first.resolved_to.should == expected_file
+    actual_search.first.resolution.should == :case_insensitive
+  end
+  
+  it "finds multiple cfmodule references" do
+    @source.content = <<-CFM
+      <cfmodule template="etc"/>
+      <cfmodule template="etal">
+    CFM
+    actual_search.should have(2).references
+  end
+  
+  it "finds the cfmodule tag without regard to case" do
+    @source.content = <<-CFM
+      <cfMODule template="whatever">
+    CFM
+    actual_search.should have(1).reference
+  end
+end
